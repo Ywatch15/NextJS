b129dd5e5d75ca94c2ea18ccc8c940b281d94905
Ywatch615 <sundram.22460@knit.ac.in>
2026-02-27 21:16:56 +0530

    take a look onto the concept of how to handle the loading state of the page while data is being fetched through server.

--- 16_data-fetching-in-server-components/src/app/todos/page.js
+++ 16_data-fetching-in-server-components/src/app/todos/page.js
@@ -1,5 +1,8 @@
-const Posts = async () => {
-  console.log(fetch);
+const Todos = async () => {
+  const slowResponse = await fetch("https://procodrr.vercel.app/?sleep=5000");
+  const data = slowResponse.json();
+
+
   const response = await fetch(
     "https://jsonplaceholder.typicode.com/todos?_limit=5"
   );
@@ -20,4 +23,29 @@ const Posts = async () => {
   );
 };
 
-export default Posts;
+export default Todos;
+
+
+// const Posts = async () => {
+//   console.log(fetch);
+//   const response = await fetch(
+//     "https://jsonplaceholder.typicode.com/todos?_limit=5"
+//   );
+//   const todos = await response.json();
+
+//   return (
+//     <>
+//       <h1>Todos</h1>
+//       <div className="todos-container">
+//         {todos.map(({ id, title, completed }) => (
+//           <div className="todo-item" key={id}>
+//             <input type="checkbox" checked={completed} readOnly />
+//             <p>{title}</p>
+//           </div>
+//         ))}
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Posts;
